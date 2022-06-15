eb205c9e46189cd2d55a8fa40e0a221b3832b8e7
gollala <gollala@gollalaui-MacBookPro.local>
2022-06-15 11:57:22 +0900

    init webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WatchExternalFilesPlugin = require('webpack-watch-files-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/init.js',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "static"),
+        },
+        watchFiles: ["src"],
+        compress: true,
+        port: 3000,
+        hot: true
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "src/index.html"}),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                  }
+                }
+            }
+        ]
+    }
+};
